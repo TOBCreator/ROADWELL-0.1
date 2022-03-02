1452683c0c0f8cd3ac7fe06a684f73cd3495b70a
TOBCreator <100687917+TOBCreator@users.noreply.github.com>
2022-03-02 14:54:44 +0800

    Add files via upload

---
+++ ROADWELL.bat
@@ -0,0 +1,756 @@
+@echo off
+cls
+color e
+set points2=0
+title ROAD WELL BY [TOB]
+
+:intro
+cls
+echo.
+echo TTTTTTTTTTTTT
+echo TTTTTTTTTTTTT
+echo     TTTTT
+echo     TTTTT
+echo     TTTTT  
+ping localhost -n 2 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   
+echo TTTTTTTTTTTTT  O    O   
+echo     TTTTT      O    O  
+echo     TTTTT      O    O   
+echo     TTTTT      OOOOOO   
+ping localhost -n 2 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+ping localhost -n 2 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo *
+echo.
+echo *
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo **
+echo.
+echo **
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo.
+echo ***
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo C
+echo ***
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo CO
+echo ***
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo T
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Te
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Tea
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team O
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of B
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Be
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Bea
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beas
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast C
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Co
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+echo *
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+echo **
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+echo ***
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+echo ****
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+echo *****
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+echo ******
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+echo *******
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+echo ********
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+echo *********
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+echo **********
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+echo ***********
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+echo ************
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+echo *************
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+echo **************
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+echo ***************
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+echo ****************
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+echo *****************
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com.
+echo ******************
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com. @
+echo ******************
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com. @2
+echo ******************
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com. @20
+echo ******************
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com. @202
+echo ******************
+ping localhost -n 1 >nul
+cls
+echo TTTTTTTTTTTTT  OOOOOO   BBBBBB
+echo TTTTTTTTTTTTT  O    O   B     B
+echo     TTTTT      O    O   B---/B
+echo     TTTTT      O    O   B     B
+echo     TTTTT      OOOOOO   BBBBBB
+echo ***
+echo COM
+echo ***
+echo.
+echo Team Of Beast Com. @2022
+echo ******************
+ping localhost -n 3 >nul
+
+:mdcall
+if exist "Data" goto menu1
+if not exist "Data" goto md
+:md
+md Data
+ping localhost -n 1 >nul
+cd Data
+(
+echo set points=0 
+)>Points.bat
+goto menu
+
+:menu1
+cd Data 
+goto menu
+
+:menu
+cls
+echo ====================
+echo Welcome to RoadWell!
+echo ====================
+echo.
+echo ------------------
+echo 1] Play
+echo 2] Blogspot
+echo 3] Exit
+echo ------------------
+echo.
+echo Team Of Beast Com. @2022
+choice /c 123 /n 
+if %errorlevel% == 1 goto play
+if %errorlevel% == 2 goto blogspot
+if %errorlevel% == 3 exit
+
+:blogspot
+cls
+set WEBSITE=chrome.exe
+set WAIT_TIME=2
+start %WEBSITE% -new "teamofbeastblogspot.blogspot.com"
+@ping 127.0.0.1 -n %WAIT_TIME% >nul
+goto menu
+
+:play
+set lor=%random%
+if %lor% GTR 5000 goto left
+if %lor% LSS 5000 goto right
+
+:left
+call Points.bat
+cls
+echo.
+echo Last Game Points=%points%
+echo Points=%points2%
+echo ===============================
+echo                        ********
+echo                        ********
+echo                        *DANGER*
+echo                        ********
+echo                        ********
+echo L = DRIVE LEFT
+echo R = DRIVE RIGHT
+echo E = BACK TO MENU
+choice /c lre /n
+if %errorlevel% == 1 goto pluspoint
+if %errorlevel% == 2 goto lose
+if %errorlevel% == 3 goto menu
+
+
+:lose
+cls
+echo ====================
+echo YOU LOST, TRY AGAIN!
+echo YOU LOST ALL YOUR POINTS!
+echo ====================
+echo.
+pause 
+set points3=%points2%
+if %points3% GTR %points% goto higherscore
+set points2=0
+goto menu
+
+:pluspoint
+cls
+echo.
+echo YOU DRIVED SAFELY!
+echo YOU EARNED [1] POINT[s]!
+echo.
+echo KEEP IT UP!
+pause >nul
+set /a points2=%points2%+1
+if %points2% GTR %points% goto higherscore
+goto play
+
+:right
+call Points.bat
+cls
+echo.
+echo Last Game Points=%points%
+echo Points=%points2%
+echo ===============================
+echo  ********
+echo  ********
+echo  *DANGER*
+echo  ********
+echo  ********
+echo L = DRIVE LEFT
+echo R = DRIVE RIGHT
+echo E = BACK TO MENU
+choice /c lr /n
+if %errorlevel% == 1 goto lose2
+if %errorlevel% == 2 goto pluspoint2
+if %errorlevel% == 3 goto menu
+
+
+:lose2
+cls
+echo ====================
+echo YOU LOST, TRY AGAIN!
+echo YOU LOST ALL YOUR POINTS!
+echo ====================
+echo.
+pause 
+set points3=%points2%
+if %points3% GTR %points% goto higherscore
+set points2=0
+goto menu
+
+:pluspoint2
+cls
+echo.
+echo YOU DRIVED SAFELY!
+echo YOU EARNED [1] POINT[s]!
+echo.
+echo KEEP IT UP!
+pause >nul
+set /a points2=%points2%+1
+if %points2% GTR %points% goto higherscore
+goto play
+
+
+
+:sh
+cls
+if %points2% == 0 goto cantsave
+if %points3% LSS %points% goto lesser
+echo SAVING CURRENT HIGHSCORE.. [%points3%]
+ping localhost -n 4 >nul
+del Data/Points.bat
+(
+echo set points=%points3%
+)>Points.bat
+goto menu
+
+:higherscore
+cls
+echo ========================
+echo YOU HAD A NEW HIGHSCORE!
+echo ========================
+echo.
+echo ------------------------
+echo LAST HIGHSCORE: %points%
+echo ------------------------
+echo NEW HIGHSCORE: %points2%
+echo ------------------------
+pause >nul
+goto sh2
+
+:sh2
+cls
+if %points2% == 0 goto cantsave
+if %points2% LSS %points% goto lesser
+echo SAVING CURRENT HIGHSCORE.. [%points2%]
+ping localhost -n 4 >nul
+del Data/Points.bat
+(
+echo set points=%points2%
+)>Points.bat
+set points2=0
+goto menu
+
+
+
+
+
+
+
+
